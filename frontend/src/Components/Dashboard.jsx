3208c1df85b18f8e4f353823463081d0bf2b5f4f
Imran Khan <99397606+imran120198@users.noreply.github.com>
2024-07-09 13:29:36 +0530

    frontend

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,28 +1,36 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import styles from "../Styles/Styles.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const fetchData = async () => {
-      const token = localStorage.getItem("token");
-      const config = {
-        headers: {
-          token: token,
-        },
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    } else {
+      const fetchData = async () => {
+        const config = {
+          headers: {
+            token: token,
+          },
+        };
+        try {
+          const res = await axios.get("http://localhost:7000/home", config);
+        } catch (err) {
+          console.log(err);
+        }
       };
-      try {
-        const res = await axios.get("http://localhost:7000/home", config);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
+      fetchData();
+    }
   }, []);
 
   return (
     <div className={styles.dashboard}>
       <h2>Dashboard</h2>
+      <p>Test Dashboard</p>
     </div>
   );
 };
